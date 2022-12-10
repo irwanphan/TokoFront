a3a2fbec18c19a4789a38be2dae35dbadd5e763a
irwanphan <irwanphan@gmail.com>
2022-12-11 00:38:20 +0700

    fixed main page

--- libs/units/CatalogFullColumn/index.tsx
+++ libs/units/CatalogFullColumn/index.tsx
@@ -10,24 +10,28 @@ interface CatalogColumnProps extends FlexProps {
     borderColor?: string
     bgColor?: string
     href: string
+    haveButton?: boolean
+    buttonText?: string
     buttonColor?: string
     buttonBorderColor?: string
     buttonTextColor?: string
 }
 
-const CatalogFullColumn = ( {title, price, currency, href, color, borderColor, bgColor, children, ...rest}: CatalogColumnProps ) => {
+const CatalogFullColumn = ( {title, price, currency, href, color, borderColor, bgColor, haveButton, buttonText, buttonColor, buttonBorderColor, buttonTextColor, ...rest}: CatalogColumnProps ) => {
     return (
         <BlockContainer {...rest}>
             <Grid templateColumns='1fr 1fr'>
                 <GridItem>
                     <Text fontSize={28} mb={4}>{title}</Text>
                     <Text mb={3}>{currency} {price}</Text>
-                    {children}
-                    <FormSubmitButton
-                        href={href}
-                        >
-                        asdf
-                    </FormSubmitButton>
+                    {
+                        haveButton &&
+                        <FormSubmitButton
+                            href={href}
+                            >
+                            {buttonText}
+                        </FormSubmitButton>
+                    }
                 </GridItem>
                 <GridItem>
                     {/* TODO: retrive picture here */}
