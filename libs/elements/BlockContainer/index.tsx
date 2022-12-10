c68fed1ce27f6aa2eb3524e9c18941330886431f
irwanphan <irwanphan@gmail.com>
2022-12-11 01:07:05 +0700

    BlockContainerLink

--- libs/elements/BlockContainer/index.tsx
+++ libs/elements/BlockContainer/index.tsx
@@ -1,5 +1,11 @@
-import { Box, FlexProps } from "@chakra-ui/react"
+import { Box, FlexProps, Text } from "@chakra-ui/react"
+import NextLink from 'next/link'
 
+interface BlockContainerLinkProps extends BlockContainerProps {
+    href: string
+    title?: string
+    description?: string
+}
 interface BlockContainerProps extends FlexProps {
     bgColor?: string
 }
@@ -23,4 +29,21 @@ const BlockContainer = ({ bgColor, children, ...rest }:BlockContainerProps) => {
         </Box>
     )
 }
+
+export const BlockContainerLink = ({ href, title, description, bgColor }:BlockContainerLinkProps) => {
+    return (
+        <NextLink href={href}>
+            {/* TODO: retrieve image here */}
+            <BlockContainer bgColor={bgColor}>
+                <Text fontSize={16} fontWeight={600}>
+                    {title}
+                </Text>
+                <Text fontSize={12} mt={1} color='blackAlpha.800'>
+                    {description}
+                </Text>
+            </BlockContainer>
+        </NextLink>
+    )
+}
+
 export default BlockContainer
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { Box, Grid, GridItem, Text } from '@chakra-ui/react'
-import BlockContainer from '@elements/BlockContainer'
+import BlockContainer, { BlockContainerLink } from '@elements/BlockContainer'
 import MainLayout from '@libs/layouts/MainLayout'
 import CatalogFullColumn from '@units/CatalogFullColumn'
+import NextLink from 'next/link'
 
 const Home = () => {
   return (
@@ -17,9 +18,7 @@ const Home = () => {
         href='/'
       />
 
-      <Box 
-        textAlign='center' 
-        mt={12} mb={8}>
+      <Box textAlign='center' mt={12} mb={8}>
         <Text fontSize={32}>
           Trending Now
         </Text>
@@ -30,14 +29,11 @@ const Home = () => {
 
       <Grid templateColumns='1fr 1fr 1fr 1fr' gap={4}>
         <GridItem>
-          <BlockContainer>
-            <Text fontSize={16} fontWeight={600}>
-              Set Your Sight Straight
-            </Text>
-            <Text fontSize={12} mt={1} color='blackAlpha.800'>
-              It's there
-            </Text>
-          </BlockContainer>
+          <BlockContainerLink href='/' 
+            title='Set Your Sight Straight'
+            description={`It's right there`}
+            bgColor='green.50'
+          />
         </GridItem>
         <GridItem>
           <BlockContainer>asdf</BlockContainer>
