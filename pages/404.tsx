2bc8a70772acbdf38a32fa8ed62e6029f574b5f9
irwanphan <irwanphan@gmail.com>
2022-12-19 16:34:42 +0700

    Custom404

---
+++ pages/404.tsx
@@ -0,0 +1,22 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import FormSubmitButton from "@elements/FormSubmit"
+import { BiRocket } from "react-icons/bi"
+
+
+const Custom404 = () => {
+    return (
+        <Box bgGradient='linear(to-b, cyan.600, blue.900)'>
+            <Flex direction='row' justifyContent='center' h='100vh'>
+                <Flex direction='column' justifyContent='center' alignItems='center' textAlign='center'>
+                    <Text fontSize={40} fontWeight={800} mb={4}>You are lost ...</Text>
+                    <FormSubmitButton href="/" >
+                        <Box as={BiRocket} fontSize={24} mr={2}/>
+                        Go Home
+                    </FormSubmitButton>
+                </Flex>
+            </Flex>
+        </Box>
+    )
+}
+
+export default Custom404
