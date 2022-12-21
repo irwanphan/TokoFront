a04b14c5b418534b7762c87f4892acd61ecb5123
irwanphan <irwanphan@gmail.com>
2022-12-22 01:57:32 +0700

    separated ManageProductPage

--- pages/admin-area/index.tsx
+++ pages/admin-area/index.tsx
@@ -4,7 +4,7 @@ import BlockContainer from "@elements/BlockContainer"
 import FormSubmitButton from "@elements/FormSubmit"
 import { CartItems } from "@libs/components/Cart"
 import MainLayout from "@libs/layouts/MainLayout"
-import { FiShoppingCart, FiPackage } from "react-icons/fi"
+import { FiShoppingCart } from "react-icons/fi"
 import { TbFileInvoice } from "react-icons/tb"
 
 
@@ -13,24 +13,12 @@ const AdminAreaPage = () => {
     
     return (
         <MainLayout>
-
             {
                 userCategory === 'admin' &&
-                <BlockContainer>
-                    <Box>
-                        <Flex alignItems='center'>
-                            <Box as={FiPackage} mr={2} />
-                            <Text fontWeight={600} >Product List</Text>
-                        </Flex>
-                        <FormSubmitButton href="/admin-area/products/create" buttonColor="green.100" >+ New</FormSubmitButton>
-                    </Box>
-                    <Divider />
-                    <Box rounded='md' border='1px solid lightgray' mt={4} p={4} shadow='sm'>
-                        <CartItems />
-                    </Box>
-                </BlockContainer>
+                <Flex gap={2}>
+                    <FormSubmitButton href="/admin-area/products">Manage Products</FormSubmitButton>
+                </Flex>
             }
-
             <Box mt={4} />
 
             <BlockContainer>
