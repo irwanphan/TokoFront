4479b7945981af950c283c673853c1d61aba9291
irwanphan <irwanphan@gmail.com>
2022-12-22 02:08:19 +0700

    protect API with next-auth session

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { getSession } from 'next-auth/react'
+
 const prisma = new PrismaClient()
 
 export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    const session = await getSession({ req })
+    if (!session) {
+        return res.status(401).json({ message: 'Unauthorized.' });
+    }
+    
     if (req.method === 'POST') {
         try {
             const { name, refId, description, price } = req.body;
