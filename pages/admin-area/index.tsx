dd87c24516fa9e38194859ad6a39740ef8438ea6
irwanphan <irwanphan@gmail.com>
2022-12-24 16:22:12 +0700

    manage products list

--- pages/admin-area/index.tsx
+++ pages/admin-area/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { Box, Divider, Flex, Text } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
+import { Box, Divider, Flex, Text, useToast } from "@chakra-ui/react"
 import BlockContainer from "@elements/BlockContainer"
 import FormSubmitButton from "@elements/FormSubmit"
 import { CartItems } from "@libs/components/Cart"
@@ -10,6 +10,9 @@ import { TbFileInvoice } from "react-icons/tb"
 // TODO: apply middleware to all admin-area
 // protect admin-area route
 import { getSession } from 'next-auth/react'
+import { loadProducts, productsState } from "@contexts/products"
+import { useRecoilState } from "recoil"
+import { ItemInterface } from "@interfaces//storeItem"
 export async function getServerSideProps(context:any) {
     // Check if user is authenticated
     const session = await getSession(context);
@@ -29,6 +32,25 @@ export async function getServerSideProps(context:any) {
 
 const AdminAreaPage = () => {
     const [ userCategory, setUserCategory ] = useState('admin')
+    const [ store, setStore ] = useRecoilState<ItemInterface[]>(productsState)
+    const [ isLoadingProducts, setIsLoadingProducts ] = useState<boolean>(true)
+    const toast = useToast()
+
+    useEffect(() => {
+        const products = loadProducts()
+        .then(res => setStore(res))
+        .then(() => setIsLoadingProducts(false))
+        .catch(e => {
+            toast({
+                title: 'Error',
+                description: `You're not connected to our server!`,
+                render: () => (
+                    <BlockContainer py={4} px={6} bgColor="green.100">You're not connected to our server!</BlockContainer>
+                )
+            })
+            // console.error(e.response.status)
+        })
+    }, [])
     
     return (
         <MainLayout>
--- pages/admin-area/products/index.tsx
+++ pages/admin-area/products/index.tsx
@@ -1,13 +1,56 @@
-import { Box, Flex, Divider, Text } from "@chakra-ui/react"
+import { Box, Flex, Divider, Text, useToast, List, ListItem, useDisclosure } from "@chakra-ui/react"
+import { productsState, loadProducts } from "@contexts/products"
 import BlockContainer from "@elements/BlockContainer"
 import FormSubmitButton from "@elements/FormSubmit"
-import { CartItems } from "@libs/components/Cart"
+import { ItemInterface } from "@interfaces//storeItem"
 import MainLayout from "@libs/layouts/MainLayout"
-import { FiPackage } from "react-icons/fi"
+import ModalPopup from "@units/ModalPopup"
+import { useState, useEffect } from "react"
+import { FiEdit, FiPackage, FiSlash } from "react-icons/fi"
+import { useRecoilState } from "recoil"
 
 const ManageProductsPage = () => {
+    const [ userCategory, setUserCategory ] = useState('admin')
+    const [ store, setStore ] = useRecoilState<ItemInterface[]>(productsState)
+    const [ isLoadingProducts, setIsLoadingProducts ] = useState<boolean>(true)
+    const toast = useToast()
+
+    useEffect(() => {
+        const products = loadProducts()
+        .then(res => setStore(res))
+        .then(() => setIsLoadingProducts(false))
+        .catch(e => {
+            toast({
+                title: 'Error',
+                description: `You're not connected to our server!`,
+                render: () => (
+                    <BlockContainer py={4} px={6} bgColor="green.100">You're not connected to our server!</BlockContainer>
+                )
+            })
+            // console.error(e.response.status)
+        })
+    }, [])
+
+    // handling delete modal
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [ scope, setScope ] = useState<ItemInterface>()
+    const modalProps = {
+        title: `Mark ${scope?.name} as Inactive`,
+        texts: 'Are you really OK with this decision?',
+        button: 'OK',
+        action: () => {
+            // handleMarkInactive(scope)
+            onClose()
+        }
+    }
+    
     return (
         <MainLayout>
+            <Flex gap={2}>
+                <FormSubmitButton href="/admin-area">Admin Dashboard</FormSubmitButton>
+            </Flex>
+            <Box mt={4} />
+
             <BlockContainer>
                 <Box>
                     <Flex alignItems='center'>
@@ -18,7 +61,73 @@ const ManageProductsPage = () => {
                 <Divider />
                 <FormSubmitButton href="/admin-area/products/create" buttonColor="green.100" >+ New</FormSubmitButton>
                 <Box rounded='md' border='1px solid lightgray' mt={4} p={4} shadow='sm'>
-                    <CartItems />
+                    <Box>
+                        <List className="cart-items">
+                            {store.map((item:ItemInterface) => {
+                                return (
+                                    <ListItem key={item.id} mb={2} >
+                                        <Flex alignItems='center' mb={1}>
+                                            {item?.name}
+                                        </Flex>
+                                        <Flex
+                                            color='gray.600'
+                                            fontSize={12}
+                                            gap={1}
+                                            justifyContent='space-between'
+                                        >
+                                            <Flex direction='column' >
+                                                <Flex gap={2}>
+                                                    <Text>Current stock</Text>                                            
+                                                    <Text fontWeight={600}>{item.currentStock}</Text>                                            
+                                                </Flex>
+                                                <Flex gap={2}>
+                                                    <Text>Price</Text>                                            
+                                                    <Text fontWeight={600}>{item.price}</Text>                                            
+                                                </Flex>
+                                            </Flex>
+                                            <Flex gap={2}>
+                                                <Flex 
+                                                    p={1} h={6}
+                                                    borderWidth='1px'
+                                                    borderStyle='solid'
+                                                    borderColor='gray.600'
+                                                    cursor='pointer'
+                                                    alignItems='center'
+                                                    direction='row'
+                                                    fontSize={12}
+                                                    transition='0.3s ease all'
+                                                    _hover={{ bgColor: 'green.200' }}
+                                                    onClick={() => {
+                                                        // setScope(item)
+                                                        // onOpen()
+                                                    }}
+                                                >Edit <Box ml={1} as={FiEdit} /></Flex>
+                                                <Flex 
+                                                    p={1} h={6}
+                                                    borderWidth='1px'
+                                                    borderStyle='solid'
+                                                    borderColor='gray.600'
+                                                    cursor='pointer'
+                                                    alignItems='center'
+                                                    direction='row'
+                                                    fontSize={12}
+                                                    transition='0.3s ease all'
+                                                    _hover={{ bgColor: 'orange.200' }}
+                                                    onClick={() => {
+                                                        setScope(item)
+                                                        onOpen()
+                                                    }}
+                                                >Mark Inactive <Box ml={1} as={FiSlash} /></Flex>
+                                            </Flex>
+                                        </Flex>
+                                        <Divider />
+                                    </ListItem>
+                                )
+                            })}
+                        </List>
+
+                        <ModalPopup modalProps={modalProps} isOpen={isOpen} onClose={onClose} />
+                    </Box>
                 </Box>
             </BlockContainer>
         </MainLayout>
