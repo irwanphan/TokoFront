fd47dab83b9e19818013f88a9b51d426672d3932
irwanphan <irwanphan@gmail.com>
2022-12-30 17:09:48 +0700

    next-auth supabase adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import { SupabaseAdapter } from "@next-auth/supabase-adapter"
 
 export default NextAuth({
     pages: {
@@ -10,10 +11,14 @@ export default NextAuth({
     },
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.GOOGLE_ID ?? '',
+            clientSecret: process.env.GOOGLE_SECRET ?? '',
         })
-    ]
+    ],
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
+    }),
 })
 
 // TODO: implement nextAuth Supabase adapter
