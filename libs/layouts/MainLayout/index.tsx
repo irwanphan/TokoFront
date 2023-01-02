70ba1eb0c07145e9d63fa6f5a610218b0e8239db
irwanphan <irwanphan@gmail.com>
2023-01-02 16:01:16 +0700

    load cart from local storage on MainLayout

--- libs/layouts/MainLayout/index.tsx
+++ libs/layouts/MainLayout/index.tsx
@@ -1,6 +1,8 @@
 import { Box, FlexProps } from "@chakra-ui/react"
+import { cartState } from "@contexts/cart"
 import { productsState, useFetchProducts } from "@contexts/products"
 import LoadingOverlay from "@elements/LoadingOverlay"
+import { CartItemInterface } from "@interfaces//cartItem"
 import { ItemInterface } from "@interfaces//storeItem"
 import TokoFooter from "@libs/components/TokoFooter"
 import { useEffect } from "react"
@@ -11,12 +13,23 @@ import CustomHeader from "../CustomHeader"
 const MainLayout = ({children, ...rest}: FlexProps) => {
     const { products, isLoadingProducts } = useFetchProducts()
 
+    // load products to store
     const [ store, setStore ] = useRecoilState<ItemInterface[]>(productsState)    
     useEffect(() => {
         isLoadingProducts   ? console.log('loading products ...')
                             : setStore(products!) 
         console.log('store update: ', store)
     }, [products])
+
+    // load cart from local-storage
+    const [ cart, setCart ] = useRecoilState<CartItemInterface[]>(cartState)
+    useEffect(() => {
+        const cartData = localStorage.getItem("cart")
+        // console.log('storage: ', cartData)
+        const parsedData = JSON.parse(cartData!)
+        if (parsedData) { setCart(parsedData) }
+        console.log('cart update: ', cart)
+    }, [])
 
     return (
         <Box
