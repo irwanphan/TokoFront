66fa2a0f6bc2c178f19853a302555b2526d45f67
irwanphan <irwanphan@gmail.com>
2023-01-02 20:40:31 +0700

    removed unused lines from TokoCatalog

--- libs/components/TokoCatalog/index.tsx
+++ libs/components/TokoCatalog/index.tsx
@@ -1,8 +1,8 @@
 import { Grid, GridItem, useToast } from "@chakra-ui/react"
 import { useEffect, useState } from 'react'
-import { loadProducts, productsState } from '@libs/contexts/products'
+import { productsState } from '@libs/contexts/products'
 import { useRecoilState } from 'recoil'
-import BlockContainer, { BlockContainerLink } from "@elements/BlockContainer"
+import { BlockContainerLink } from "@elements/BlockContainer"
 import { ItemInterface } from "@libs/interfaces/storeItem"
 import LoadingBlock from "@elements/LoadingBlock"
 
@@ -13,20 +13,8 @@ const TokoCatalog = () => {
     const [ isLoadingProducts, setIsLoadingProducts ] = useState<boolean>(true)
 
     useEffect(() => {
-        const products = loadProducts()
-        .then(res => setStore(res))
-        .then(() => setIsLoadingProducts(false))
-        .catch(e => {
-            toast({
-                title: 'Error',
-                description: `You're not connected to our server!`,
-                render: () => (
-                    <BlockContainer py={4} px={6} bgColor="green.100">You're not connected to our server!</BlockContainer>
-                )
-            })
-            // console.error(e.response.status)
-        })
-    }, [])
+        setIsLoadingProducts(false)
+    }, [store])
     
     if (isLoadingProducts) return (
         <Grid templateColumns={{base: '1fr', sm:'1fr 1fr', md: '1fr 1fr 1fr', lg: '1fr 1fr 1fr 1fr'}} gap={4}>
