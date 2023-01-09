17d5a8bf6df04f6db3a96c001d5b9a5bbef41804
irwanphan <irwanphan@gmail.com>
2023-01-10 01:44:16 +0700

    custom hook useFetchUsers

---
+++ libs/hooks/useFetchUsers.ts
@@ -0,0 +1,26 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+export const useFetchUsers = () => {
+    const [ users, setUsers ] = useState()
+    const [ isLoadingUsers, setIsLoadingUsers ] = useState<boolean>(true)
+    // console.log(users)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data: response } = await axios.get('/api/users')
+                setUsers(response)
+            } catch (error) {
+                console.log(error)
+            }
+            setIsLoadingUsers(false)
+        }
+        fetchData()
+    }, [])
+
+    return {
+        users,
+        isLoadingUsers
+    }
+}
