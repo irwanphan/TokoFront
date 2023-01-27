e459acbb229b4c304bcd5493955c5fc1cc00c2ee
irwanphan <irwanphan@gmail.com>
2023-01-28 01:20:18 +0700

    redirect after login

--- libs/connections/signIn.ts
+++ libs/connections/signIn.ts
@@ -9,7 +9,7 @@ const getURL = () => {
     let url =
         process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
         process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        'http://localhost:3000/'
+        'https://tokofront.vercel.app/'
         // Make sure to include `https://` when not localhost.
         url = url.includes('http') ? url : `https://${url}`
         // Make sure to including trailing `/`.
@@ -21,7 +21,7 @@ export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            // redirectTo: getURL()
+            redirectTo: getURL()
         }
     })
 }
