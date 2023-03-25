f1e347c052b21e34dc6e7e36e67f8c708c3cfc7b
irwanphan <irwanphan@gmail.com>
2023-03-25 14:12:37 +0700

    settings GET API

---
+++ pages/api/settings/index.ts
@@ -0,0 +1,54 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@libs/connections/prisma'
+
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+    if (req.method === 'GET') {
+        try {
+            const settings = await prisma.setting.findMany()
+            console.log(settings)
+            return res.status(200).json(settings)
+        }
+        catch (e) {
+            console.log(e)
+            return res.status(500).json({ message: `${e}` })
+        }
+    }
+    
+    // if (req.method === 'POST') {
+        
+    //     try {
+    //         const { id, email, name, image } = req.body
+
+    //         const existingUser = await prisma.user.findUnique({
+    //             where: { email: email }
+    //         })
+    //         console.log(existingUser)
+
+    //         const code = id
+
+    //         if (!existingUser) {
+    //             const user = await prisma.user.create({
+    //                 data: {
+    //                     code,
+    //                     email,
+    //                     name,
+    //                     image
+    //                 }
+    //             })
+    //             // console.log(user)
+    //             return res.status(200).json(user)
+    //         }
+    //         return res.status(200).json(existingUser)
+
+    //     } catch (e:any) {
+    //         console.log(e)
+    //         return res.status(500).json({ message: `${e.status}` })
+    //     }
+    // }
+    // else {
+    //     res.setHeader('Allow', ['POST']);
+    //     return res
+    //         .status(405)
+    //         .json({ message: `HTTP method ${req.method} is not supported.` })
+    // }
+}
