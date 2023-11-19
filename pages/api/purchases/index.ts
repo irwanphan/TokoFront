4775db5596510faa5d268b51bd88885c0548c718
irwanphan <irwanphan@gmail.com>
2023-11-19 16:08:16 +0700

    remove shipment

--- pages/api/purchases/index.ts
+++ pages/api/purchases/index.ts
@@ -80,7 +80,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse) {
                         data: {
                             currentStock: { increment: order.quantity },
                             lastPurchasePrice: order.lastPurchasePrice,
-                            updatedAt: ((new Date()).toISOString()).toLocaleString()
+                            updatedAt: ((new Date()).toISOString())
                         },
                     });
                   
