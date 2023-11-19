0239ccda714b88644c32398f5bced05851ec93c3
irwanphan <irwanphan@gmail.com>
2023-11-19 16:57:48 +0700

    by id

--- pages/api/purchases/get-all-by-user-id.ts
+++ pages/api/purchases/get-all-by-user-id.ts
@@ -1,28 +1,34 @@
-import prisma from '@libs/connections/prisma'
-import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@libs/connections/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    const query:any = req.query
-    const userId:any = query.userId
-    
-    // get all purchases
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const query: any = req.query;
+    const userId: any = query.userId;
+
+    // Check if the user is authenticated
+    if (!userId) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    // get all purchases for the logged-in user
     if (req.method === 'GET') {
         try {
             const purchases = await prisma.purchase.findMany({
+                where: {
+                    userId: userId,
+                },
                 include: {
                     detail: true,
-                    shipment: true
+                    shipment: true,
                 },
                 orderBy: {
-                    id: 'desc'
-                }
-            })
-            // console.log(purchases)
-            return res.status(200).json(purchases)
-        }
-        catch (e) {
-            console.log(e)
-            return res.status(500).json({ message: `${e}` })
+                    id: 'desc',
+                },
+            });
+            return res.status(200).json(purchases);
+        } catch (e) {
+            console.error(e);
+            return res.status(500).json({ message: `${e}` });
         }
     }
 }
